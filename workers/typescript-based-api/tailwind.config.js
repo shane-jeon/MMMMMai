4ad03010eda25e238821d615ec439d1e2dcc2ab1
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-09-07 14:59:54 -0700

    styling changes on frontend

---
+++ workers/typescript-based-api/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./*.html', // Include HTML files
+		'./*.js', // Include JavaScript files
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
